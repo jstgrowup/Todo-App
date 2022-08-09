50eeaaf58dec4fbd4ac33b569aebc2699749f3e6
jstgrowup <deysubham999@gmail.com>
2022-08-09 08:29:39 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-// import './App.css'
+
 import Todo from "./Components/Todo";
 import { ChakraProvider } from "@chakra-ui/react";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <ChakraProvider>
